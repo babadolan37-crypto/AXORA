ace11714acd6ebf624ca1431c9ce8337931f4f82
babadolan37-crypto <babadolan37@gmail.com>
2025-12-24 23:36:31 +0700

    Move Analytics to main.tsx and cleanup App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -157,8 +157,7 @@ function App() {
         <div className="text-center">
           <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600 mx-auto mb-4"></div>
           <p className="text-gray-600">Memuat...</p>
-          <Analytics />
-    </div>
+        </div>
       </div>
     );
   }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App.tsx";
 import "./index.css";
 import { ErrorBoundary } from "./components/ErrorBoundary";
@@ -26,6 +27,7 @@ try {
     <React.StrictMode>
       <ErrorBoundary>
         <App />
+        <Analytics />
       </ErrorBoundary>
     </React.StrictMode>
   );
